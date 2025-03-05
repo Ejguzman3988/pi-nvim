ee4eef274188fc273526734a9aee7981fe3cd358
Eriberto Guzman <eriberto.guzman@outlook.com>
2025-03-04 19:36:54 -0500

    set up telescope and now on advanced telescope

--- init.lua
+++ init.lua
@@ -1,26 +1,23 @@
 require("config.lazy")
 
-print("FILE SOURCED")
-
 local set = vim.opt
 set.shiftwidth = 2
+set.tabstop = 4
 set.number = true
 set.relativenumber = true
 set.clipboard = "unnamedplus"
 
-
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", {desc = "Source current file"})
-vim.keymap.set("n", "<space>x", ":.lua<CR>",{ desc = "Execute current line"})
-vim.keymap.set("v", "<space>x", ":lua<CR>",{ desc = "Execute highlighted line"})
-
-vim.keymap.set("i", "jk", "<ESC>", { desc = "Alternative Esacpe" })
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", { desc = "Source current file" })
+vim.keymap.set("n", "<space>x", ":.lua<CR>", { desc = "Execute current line" })
+vim.keymap.set("v", "<space>x", ":lua<CR>", { desc = "Execute highlighted line" })
+vim.keymap.set('n', '<space>e', ':Ex<CR>', { desc = "Go to explorer" })
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Alternative Escape" })
 
 -- Highlight when yanking text
-
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank',{ clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = 'Highlight when yanking text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+-- init.lua:
+-- plugins/telescope.lua:
+return {
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
+	-- or                              , branch = '0.1.x',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+	},
+	config = function()
+		require('telescope').setup {
+			pickers = {
+				find_files = {
+					theme = "ivy"
+				}
+			}
+		}
+		-- print("")
+		vim.keymap.set('n', '<space>fh', require('telescope.builtin').help_tags)
+		vim.keymap.set('n', '<space>ff', require('telescope.builtin').find_files)
+		vim.keymap.set('n', '<space>fc', function()
+			require('telescope.builtin').find_files {
+				cwd = vim.fn.stdpath("config")
+			}
+		end)
+	end
+}
