a2139b7ee2493e03d273351d58306ddc7888c0f1
Eriberto Guzman <eriberto.guzman@outlook.com>
2025-03-12 10:30:52 -0400

    added new keymaps

--- init.lua
+++ init.lua
@@ -1,21 +1,22 @@
 require("config.lazy")
 require("config.options")
 
-
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", { desc = "Source current file" })
 vim.keymap.set("n", "<space>x", ":.lua<CR>", { desc = "Execute current line" })
 vim.keymap.set("v", "<space>x", ":lua<CR>", { desc = "Execute highlighted line" })
-vim.keymap.set('n', '<space>e', ':Oil<CR>', { desc = "Go to explorer" })
+vim.keymap.set("n", "<space>e", ":Oil<CR>", { desc = "Go to explorer" })
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Alternative Escape" })
 
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Change window up" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Change window down" })
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Change window left" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Change window right" })
+vim.keymap.set("n", "<space>-", "<C-w>_", { desc = "Full screen Window" })
+vim.keymap.set("n", "<space>=", "<C-w>=", { desc = "Equal size window" })
 
-vim.keymap.set('n', "<M-o>", "<cmd>copen<CR>", { desc = "Quickfix open" })
-vim.keymap.set('n', "<M-j>", "<cmd>cnext<CR>", { desc = "Quickfix Next" })
-vim.keymap.set('n', "<M-k>", "<cmd>cnprev<CR>", { desc = "Quickfix prev" })
+vim.keymap.set("n", "<M-o>", "<cmd>copen<CR>", { desc = "Quickfix open" })
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>", { desc = "Quickfix Next" })
+vim.keymap.set("n", "<M-k>", "<cmd>cnprev<CR>", { desc = "Quickfix prev" })
 
 vim.keymap.set("n", "<S-h>", ":bprevious<CR>", { desc = "Move Tab Left", noremap = true, silent = true })
 vim.keymap.set("n", "<S-l>", ":bnext<CR>", { desc = "Move Tab Right", noremap = true, silent = true })
@@ -23,26 +24,22 @@ vim.keymap.set("n", "<S-l>", ":bnext<CR>", { desc = "Move Tab Right", noremap = 
 -- git
 vim.keymap.set("n", "<leader>ghd", "<cmd>lua MiniDiff.toggle_overlay()<CR>", { desc = "Toggle diff overlay" })
 
-
 -- Highlight when yanking text
-vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
 })
 
-
-vim.api.nvim_create_autocmd('TermOpen', {
-	group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
 	callback = function()
 		vim.opt.number = false
 		vim.opt.relativenumber = false
 	end,
 })
-
-
 
 -- local job_id = 0
 
@@ -65,16 +62,6 @@ vim.diagnostic.config({
 	underline = true,        -- Underline problematic code
 	update_in_insert = false, -- Avoid updates while typing
 })
-
-
-vim.notify = require("mini.notify").make_notify()
-
-vim.lsp.handlers["window/showMessage"] = function(_, result, ctx)
-	local client = vim.lsp.get_client_by_id(ctx.client_id)
-	require("mini.notify").make_notify()(result.message, ({
-		"ERROR", "WARN", "INFO", "DEBUG",
-	})[result.type], { title = client and client.name or "LSP" })
-end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 	vim.lsp.diagnostic.on_publish_diagnostics,
