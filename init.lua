bf7a84037644eb6f8839cc19bac378c6f2248105
Eriberto Guzman <eriberto.guzman@outlook.com>
2025-03-11 20:52:08 -0400

    add the rest of the options up to custom right click menus in neovim

--- init.lua
+++ init.lua
@@ -1,17 +1,22 @@
 require("config.lazy")
+require("config.options")
 
-local set = vim.opt
-set.shiftwidth = 2
-set.tabstop = 4
-set.number = true
-set.relativenumber = true
-set.clipboard = "unnamedplus"
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", { desc = "Source current file" })
 vim.keymap.set("n", "<space>x", ":.lua<CR>", { desc = "Execute current line" })
 vim.keymap.set("v", "<space>x", ":lua<CR>", { desc = "Execute highlighted line" })
-vim.keymap.set('n', '<space>e', ':Ex<CR>', { desc = "Go to explorer" })
+vim.keymap.set('n', '<space>e', ':Oil<CR>', { desc = "Go to explorer" })
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Alternative Escape" })
+
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Change window up" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Change window down" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Change window left" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Change window right" })
+
+vim.keymap.set('n', "<M-o>", "<cmd>copen<CR>", { desc = "Quickfix open" })
+vim.keymap.set('n', "<M-j>", "<cmd>cnext<CR>", { desc = "Quickfix Next" })
+vim.keymap.set('n', "<M-k>", "<cmd>cnprev<CR>", { desc = "Quickfix prev" })
+
 
 -- Highlight when yanking text
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -20,4 +25,37 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 	callback = function()
 		vim.highlight.on_yank()
 	end,
+})
+
+
+vim.api.nvim_create_autocmd('TermOpen', {
+	group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+	end,
+})
+
+
+
+-- local job_id = 0
+
+vim.keymap.set("n", "<space>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 15)
+
+	-- job_id = vim.bo.channel
+end)
+
+-- vim.keymap.set("n", "<space>test", function()
+-- 	vim.fn.chansend(job_id, { "echo 'hi'\r\n" })
+-- end)
+--
+vim.diagnostic.config({
+	virtual_text = true,     -- Show diagnostics inline
+	signs = true,            -- Keep signs in the sign column
+	underline = true,        -- Underline problematic code
+	update_in_insert = false, -- Avoid updates while typing
 })
---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Neovim options with defaults, explanations, and possible values
+local opt = vim.opt
+opt.shiftwidth = 2
+opt.tabstop = 4
+opt.number = true
+opt.relativenumber = true
+opt.clipboard = "unnamedplus"
+
+return opt
